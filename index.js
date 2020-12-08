1eb737d59bd35ac833ac3f1dbfe8ec63ec5916b8
seiga al ghifari <seigaalghi@gmail.com>
2020-12-08 17:11:36 +0700

    ready to deploy

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const router = require('./src/routes');
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/uploads', express.static('uploads'));
-
 app.use('/api/v1', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Express is running at port ${PORT}`));
