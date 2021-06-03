452e481ce97b88b870c0658815e4b25470e4d563
seiga al ghifari <seigaalghi@gmail.com>
2021-06-03 12:59:09 +0700

    ! db url

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "admin",
+    database: "coways2",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    protocol: "postgres",
+  },
+};
